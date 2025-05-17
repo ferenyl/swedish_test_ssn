ff3204650ea2cad7750895927ba8e91c8a2269dc
Ferenyl <ahlback.johan@gmail.com>
2025-05-17 22:23:52 +0200

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Swedish test social security numbers
+
+Get Swedish test ssn from The Swedish Tax Agency (Skatteverket).
+
+Usage: swedish_test_ssn [OPTIONS]
+
+Options:
+-p, --pattern [default: .*]
+-l, --limit [default: 100]
+-o, --offset [default: 0]
+-j, --json
+-h, --help Print help
+-V, --version Print version
---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Result {
+    result_count: i32,
+    offset: i32,
+    limit: i32,
+    query_time: i32,
+    results: Vec<SsnResult>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct SsnResult {
+    testpersonnummer: String,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value_t = String::from(".*"))]
+    pattern: String,
+
+    #[arg(short, long, default_value_t = 100)]
+    limit: i32,
+
+    #[arg(short, long, default_value_t = 0)]
+    offset: i32,
+
+    #[arg(short, long, default_value_t = false)]
+    json: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    let pattern = args.pattern;
+    let limit = args.limit;
+    let offset = args.offset;
+    let url = format!(
+        "https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763?testpersonnummer={pattern}&_limit={limit}&_offset={offset}"
+    );
+
+    let response = reqwest::blocking::get(url).unwrap();
+    let result: Result = response.json().unwrap();
+    let items: Vec<String> = result
+        .results
+        .iter()
+        .map(|v| v.testpersonnummer.to_string())
+        .collect();
+
+    match args.json {
+        false => {
+            for item in items {
+                println!("{}", item);
+            }
+        }
+        true => {
+            let json_str = serde_json::to_string(&items).unwrap();
+            println!("{}", json_str);
+        }
+    }
+}
