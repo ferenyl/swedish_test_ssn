2a23a4c41857d83d5c0a67675da4c09ad1d450c5
Ferenyl <ahlback.johan@gmail.com>
2025-05-29 10:04:53 +0200

    better errors

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-struct Result {
+struct ApiResult {
     result_count: i32,
     offset: i32,
     limit: i32,
@@ -18,54 +18,68 @@ struct SsnResult {
 }
 
 #[derive(Parser)]
-#[command(version, about, long_about = None, about = String::from("Get swedish test ssn from The Swedish Tax Agency (Skatteverket). Great for testing without risking breaching GDPR"))]
+#[command(
+    version,
+    about,
+    long_about = None,
+    about = "Get swedish test ssn from The Swedish Tax Agency (Skatteverket). Great for testing without risking breaching GDPR"
+)]
 struct Args {
-    #[arg(short, long, default_value_t = String::from(".*"), help = String::from("Pattern for ssn. Regular expressions can be used"))]
+    #[arg(short, long, default_value_t = String::from(".*"), help = "Pattern for ssn. Regular expressions can be used")]
     pattern: String,
 
-    #[arg(short, long, default_value_t = 100, help = String::from("Limit the number of items returned"))]
+    #[arg(
+        short,
+        long,
+        default_value_t = 100,
+        help = "Limit the number of items returned"
+    )]
     limit: i32,
 
-    #[arg(short, long, default_value_t = 0, help = String::from("Number of items to skip"))]
+    #[arg(short, long, default_value_t = 0, help = "Number of items to skip")]
     offset: i32,
 
-    #[arg(short, long, default_value_t = false, help = String::from("Return as json array"))]
+    #[arg(short, long, default_value_t = false, help = "Return as json array")]
     json: bool,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let url = format!(
-        "https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763?testpersonnummer={}&_limit={}&_offset={}",
-        args.pattern, args.limit, args.offset
-    );
+    match get_items(&args) {
+        Ok(items) => print_items(args.json, &items),
+        Err(e) => eprintln!("Failed to fetch items: {}", e),
+    }
 
-    let items = get_items(&url);
-
-    print_items(args.json, items);
+    Ok(())
 }
 
-fn print_items(json: bool, items: Vec<String>) {
-    match json {
-        false => {
-            for item in items {
-                println!("{}", item);
-            }
-        }
-        true => {
-            let json_str = serde_json::to_string_pretty(&items).unwrap();
-            println!("{}", json_str);
+fn print_items(json: bool, items: &[String]) {
+    if json {
+        let json_str = serde_json::to_string_pretty(items).unwrap();
+        println!("{}", json_str);
+    } else {
+        for item in items {
+            println!("{}", item);
         }
     }
 }
 
-fn get_items(url: &str) -> Vec<String> {
-    let response = reqwest::blocking::get(url).unwrap();
-    let result: Result = response.json().unwrap();
-    result
+fn get_items(args: &Args) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let url = format!(
+        "https://skatteverket.entryscape.net/rowstore/dataset/b4de7df7-63c0-4e7e-bb59-1f156a591763?testpersonnummer={}&_limit={}&_offset={}",
+        args.pattern, args.limit, args.offset
+    );
+    let response = reqwest::blocking::get(url)?;
+    let result: ApiResult = response.json()?;
+
+    if result.results.is_empty() {
+        Err(format!("No result for pattern: {}", args.pattern))?
+    }
+
+    Ok(result
         .results
-        .iter()
-        .map(|v| v.testpersonnummer.clone())
-        .collect()
+        .into_iter()
+        .map(|v| v.testpersonnummer)
+        .collect())
 }
